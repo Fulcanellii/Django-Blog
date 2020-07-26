e0594bcfab8ed684d20f4c3b378d7d162f41a4bd
Kulmambetov Atay <a.kulmambetov@yandex.ru>
2020-07-26 12:31:39 +0600

    render Posts on main page

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from Blog.models import Post
 
-# Create your views here.
+class PostsListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    ordering = ['-date_posted']
+    context_object_name = 'posts'
+    paginate_by = 5
+
